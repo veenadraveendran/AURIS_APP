ce26dc76668c46728b2ac6b709390c34ef6192f5
Admin <veenadraveendran@gmail.com>
2019-05-09 12:15:03 +0530

    deaf web service user insert pojo 1  commit

---
+++ app/src/main/java/com/admin/auris_updated/InsertPojo.java
@@ -0,0 +1,18 @@
+package com.admin.auris_updated;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class InsertPojo {
+    @SerializedName("error")
+    @Expose
+    private Boolean error;
+
+    public Boolean getError() {
+        return error;
+    }
+    public void setError(Boolean error) {
+        this.error = error;
+    }
+
+}
