ec6019cd010b1c83a9f5e2734cab5786c1de39b0
Admin <veenadraveendran@gmail.com>
2019-05-09 12:12:02 +0530

    deaf web service user login2  commit

--- app/src/main/java/com/admin/auris_updated/WebService/getRetrofitInstance.java
+++ app/src/main/java/com/admin/auris_updated/WebService/getRetrofitInstance.java
@@ -4,6 +4,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class getRetrofitInstance {
+
     private static Retrofit retrofit;
     private static final String BASE_URL = "http://auris.nyesteventuretech.com/android/";
 
