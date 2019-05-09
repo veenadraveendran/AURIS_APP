ce45c25a9df5c39ecdee9a167cc1cee857956a15
Admin <veenadraveendran@gmail.com>
2019-05-09 12:39:16 +0530

    mainactivity4

--- app/src/main/java/com/admin/auris_updated/PartialDeaf/MainActivityPartialDeaf.java
+++ app/src/main/java/com/admin/auris_updated/PartialDeaf/MainActivityPartialDeaf.java
@@ -66,11 +66,6 @@ public class MainActivityPartialDeaf extends AppCompatActivity
 
 
 
-
-
-
-
-
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
