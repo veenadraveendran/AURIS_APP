e529c29bb052e35857dfd852649ff47a3a731df1
Admin <veenadraveendran@gmail.com>
2019-05-16 14:52:21 +0530

    emergency receiver

---
+++ app/src/main/java/com/admin/auris_updated/Receiver/Emergencyreceiver.java
@@ -0,0 +1,24 @@
+package com.admin.auris_updated.Receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import com.admin.auris_updated.PartialDeaf.AlertDialogueEm;
+import com.admin.auris_updated.Utilites;
+
+public class Emergencyreceiver extends BroadcastReceiver {
+
+
+    @Override
+    public void onReceive(final Context context, Intent intent) {
+        if(Utilites.getSharedPrferencedata(context,"ems").equals("1")) {
+            Intent i = new Intent(context, AlertDialogueEm.class);
+            i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+
+        }
+    }
+
+
+}
