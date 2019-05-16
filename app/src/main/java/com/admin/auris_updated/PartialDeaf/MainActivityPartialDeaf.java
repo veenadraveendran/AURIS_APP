bb1d1d2995a575e0d337720cdc05c0c1f8595914
Admin <veenadraveendran@gmail.com>
2019-05-16 14:49:47 +0530

    insert pojo corrected

--- app/src/main/java/com/admin/auris_updated/PartialDeaf/MainActivityPartialDeaf.java
+++ app/src/main/java/com/admin/auris_updated/PartialDeaf/MainActivityPartialDeaf.java
@@ -215,11 +215,11 @@ public class MainActivityPartialDeaf extends AppCompatActivity
             i.setType("video/*");
             startActivityForResult(i, SELECT_VIDEO);
         }
-       /* else if(id==R.id.amplify){
+        else if(id==R.id.amplify){
             Intent i = new Intent(Intent.ACTION_PICK, MediaStore.Video.Media.INTERNAL_CONTENT_URI);
             i.setType("video/*");
             startActivityForResult(i, SELECT_VIDEO);
-        }*/
+        }
             else if (id == R.id.talk) {
             Intent intent = new Intent(getApplicationContext(), TexttoSpeech.class);
             startActivity(intent);
